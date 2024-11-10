3676e82dff10b54e0998c660605a6d0a151953a6
RaviPatel021 <ravi_patel@utexas.edu>
2024-11-10 05:05:00 -0600

    added speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import { RockPaperScissorsComponent } from "@/components/rock-paper-scissors"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export default function Page() {
-  return <RockPaperScissorsComponent />
+  return (
+    <>
+      <RockPaperScissorsComponent />
+      <SpeedInsights />
+    </>
+  );
 }
